b0a3f5c4cf158cf35912714ea62f21db18483af6
Aasrith-Lead <leadaasrith@gmail.com>
2025-08-02 09:24:05 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class wrapper {
+public static void main(String args[]) {
+	Integer a = 564;
+	Boolean b = true;
+	Double d = 9.976d;
+	int c = 10;
+	System.out.println(a.toString().length());
+	System.out.println(b.toString().length());
+	System.out.println(d.toString().length());
+	System.out.println(c);
+	System.out.println(a);
+}
+}

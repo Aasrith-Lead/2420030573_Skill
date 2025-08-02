9dbbf9c890090e29437e1be61dc5c55e84332dcc
Aasrith-Lead <leadaasrith@gmail.com>
2025-08-02 09:23:16 +0530

    Add files via upload

---
+++ AutoCast.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+public class AutoCast {
+	public static void main(String args[]) {
+		
+		double a=10.568;
+		int b=(int)a;
+		
+		System.out.println("The number is : "+a);
+		System.out.println("The double num is : "+b);
+	}
+}
+
